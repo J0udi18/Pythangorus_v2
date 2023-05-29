cefe9ff20f750e998de8bb587a518fd87996590b
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-05-29 22:19:32 +1200

    Add files via upload

---
+++ Simple_pythagoras_quiz.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+def getLength() :
+    length = float(input("please enter length of Traiangle side"))
+    return length
+
+side1 = getLength ()
+side2 = getLength ()
+
+def calcTriangle(side1, side2):
+    side3 = sqrt(side1 * side1 + side2 * side2)
+    print("The length of your third side is: %.2f" % side3)
+
+
+calcTriangle(side1, side2)
