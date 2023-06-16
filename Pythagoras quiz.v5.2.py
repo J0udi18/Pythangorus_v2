a88df81c41253eef13170380632cc9734514a6b7
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-06-16 18:39:01 +1200

    Add files via upload

--- Pythagoras quiz.v5.2.py
+++ Pythagoras quiz.v5.2.py
@@ -2,7 +2,6 @@ import random
 import time
 from termcolor import colored
 
-
 def instructions():
     print(colored("Welcome to the Pythagorean Theorem Quiz!", "blue"))
     print("Instructions: In each round, a random right triangle will be generated.")
@@ -10,7 +9,6 @@ def instructions():
     print("After each answer, feedback will be provided on whether your answer was correct or not.")
     print()
 
-# Timer function stalls program and counts down
 def timer(t):
     time_set = yes_no("Would you like a timer? ")
     if time_set == "yes":
@@ -18,36 +16,24 @@ def timer(t):
     else:
         time_limit = float("inf")  # Set an infinite time limit
 
-
-# checks how many round had the player played
 def check_rounds(round_error=None):
     while True:
         response = input("How many rounds: ")
 
-        rounds_error = "please type either <enter> or an " \
-                       "integer that is more than 0\n"
+        rounds_error = "Please type either <enter> or an integer that is more than 0.\n"
 
-        # If infinite mode not chosen, check response
-        # is an integer that is more than 0
         if response != "":
             try:
                 response = int(response)
 
-                # If response is too low, go back to
-                # start of loop
                 if response < 1:
                     print(rounds_error)
                     continue
 
-              except ValueError:
+            except ValueError:
                 print(round_error)
                 continue
             return response
-
-
-            # If response is not an integer go back to
-            # start of loop
-
 
 def generate_question():
     a = random.randint(1, 10)
@@ -58,7 +44,6 @@ def generate_question():
     answer = str(c)
     return question, answer
 
-
 def print_feedback(correct):
     if correct:
         feedback = colored("Correct!", "green")
@@ -66,19 +51,16 @@ def print_feedback(correct):
         feedback = colored("Incorrect.", "red")
     print(feedback)
 
-
 def yes_no(question, error, num_type, exit_code=None, low=None, high=None):
     valid = False
     while not valid:
         try:
-            # Checks if user inputs exit code
             response = input(question)
             if response == exit_code:
                 return response
             else:
                 response = num_type(response)
 
-            # Checks if they inputted correct number
             if low is not None and high is not None:
                 if low < response < high:
                     return response
@@ -102,4 +84,4 @@ def yes_no(question, error, num_type, exit_code=None, low=None, high=None):
             print(error)
             print()
 
-        print("Thank you for taking the Pythagorean Theorem Quiz!")
+    print("Thank you for taking the Pythagorean Theorem Quiz!")
